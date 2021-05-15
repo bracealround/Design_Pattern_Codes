686185f7a6e4ea11eb88b95d901342cd746214be
Sourav Saha <40169851+bracealround@users.noreply.github.com>
2021-05-16 04:24:55 +0600

    Delete Liskov directory

--- Liskov/Efficient/SoftDrinks.java
+++
@@ -1,8 +0,0 @@
-public abstract class SoftDrinks {
-    int sugar,water;
-    abstract int addsugar();
-    abstract Integer addwater();
-    abstract void make_softdrinks();
-    abstract Object add_sweetnessingredient();
-    abstract int add_foodcolor();
-}
--- Liskov/Inefficient/SoftDrinks.java
+++
@@ -1,7 +0,0 @@
-//Here SoftDrinks is the template parent class.
-public abstract class SoftDrinks {
-    int sugar,water;
-    abstract int addsugar();
-    abstract Integer addwater();
-    abstract void make_softdrinks();
-}
